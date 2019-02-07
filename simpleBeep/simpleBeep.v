2dad6aaad9dbc66f8df719e65f411affe027bc19
yoheikoga <prawv2@gmail.com>
2019-02-07 21:32:09 +0900

    first commit

---
+++ simpleBeep/simpleBeep.v
@@ -0,0 +1,20 @@
+// look in pins.pcf for all the pin names on the TinyFPGA BX board
+module music (
+    input CLK,    // 16MHz clock
+    output USBPU,  // USB pull-up resistor
+    output SPEAKER   // use beep, // output PIN_1
+);
+    // drive USB pull-up resistor to '0' to disable USB
+    assign USBPU = 0;
+
+    // first create a 16bit binary counter
+    reg[15:0] counter;
+    always @(posedge CLK) begin
+        counter <= counter+1;
+    end
+
+    // tinyFPGA BX is 16MHz, so build 16MHz clocked 16bit free running counter
+    // 16000000/65536 = 244Hz
+    // and use the most significant bit(MSB) of the counter to drive the SPEAKER
+    assign SPEAKER = counter[15];
+endmodule
